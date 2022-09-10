305ee078b5010b6d5a6bd490eeee823e34aa95d0
mariagracialg <84107305+mariagracialg@users.noreply.github.com>
2022-09-09 19:35:07 -0500

    Improve .js code

--- script.js
+++ script.js
@@ -45,65 +45,78 @@ function changeLocation(event) {
   let apiKey = `33ff0888d56d2589acf1d714577ae3c1`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=${units}`;
 
-  function currentTemperature(response) {
-    let todayTemp = document.querySelector(`#today-degrees`);
-    let temp = Math.round(response.data.main.temp);
-    todayTemp.innerHTML = `${temp}°C`;
-
-    let weatherDescription = document.querySelector(`#today-temp-description`);
-    let description = response.data.weather[0].description;
-    weatherDescription.innerHTML = description;
-
-    let todayIcon = document.querySelector(`#today-weather-icon`);
-    todayIcon.setAttribute(
-      `src`,
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-    todayIcon.setAttribute(`alt`, response.data.weather[0].description);
-
-    let minTemp = document.querySelector(`#min-temp`);
-    let min = Math.round(response.data.main.temp_min);
-    minTemp.innerHTML = min;
-
-    let maxTemp = document.querySelector(`#max-temp`);
-    let max = Math.round(response.data.main.temp_max);
-    maxTemp.innerHTML = max;
-
-    let realFeel = document.querySelector(`#real-feel`);
-    let feelsLike = Math.round(response.data.main.feels_like);
-    realFeel.innerHTML = feelsLike;
-
-    let humidityCard = document.querySelector(`#humidity`);
-    let humidity = Math.round(response.data.main.humidity);
-    humidityCard.innerHTML = humidity;
-
-    let windSpeedCard = document.querySelector(`#wind-speed`);
-    let windSpeed = Math.round(response.data.wind.speed);
-    windSpeedCard.innerHTML = windSpeed;
-
-    let sunriseCard = document.querySelector(`#sunrise`);
-    let sunriseUnix = response.data.sys.sunrise;
-    let sunriseTime = new Date(sunriseUnix * 1000);
-    let sunriseHours = String(sunriseTime.getHours()).padStart(2, `0`);
-    let sunriseMinutes = String(sunriseTime.getMinutes()).padStart(2, `0`);
-    let sunriseSeconds = String(sunriseTime.getSeconds()).padStart(2, `0`);
-    let sunrise = `${sunriseHours}:${sunriseMinutes}:${sunriseSeconds}`;
-    sunriseCard.innerHTML = sunrise;
-
-    let sunsetCard = document.querySelector(`#sunset`);
-    let sunsetUnix = response.data.sys.sunset;
-    let sunsetTime = new Date(sunsetUnix * 1000);
-    let sunsetHours = String(sunsetTime.getHours()).padStart(2, `0`);
-    let sunsetMinutes = String(sunsetTime.getMinutes()).padStart(2, `0`);
-    let sunsetSeconds = String(sunsetTime.getSeconds()).padStart(2, `0`);
-    let sunset = `${sunsetHours}:${sunsetMinutes}:${sunsetSeconds}`;
-    sunsetCard.innerHTML = sunset;
-
-    console.log(response);
-  }
-
   axios.get(apiUrl).then(currentTemperature);
 }
+
+function currentTemperature(response) {
+  let currentCity = document.querySelector(`#current-city`);
+  let city = response.data.name;
+  currentCity.innerHTML = city;
+
+  let todayTemp = document.querySelector(`#today-degrees`);
+  let temp = Math.round(response.data.main.temp);
+  todayTemp.innerHTML = `${temp}°C`;
+
+  let weatherDescription = document.querySelector(`#today-temp-description`);
+  let description = response.data.weather[0].description;
+  weatherDescription.innerHTML = description;
+
+  let todayIcon = document.querySelector(`#today-weather-icon`);
+  todayIcon.setAttribute(
+    `src`,
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  todayIcon.setAttribute(`alt`, response.data.weather[0].description);
+
+  let minTemp = document.querySelector(`#min-temp`);
+  let min = Math.round(response.data.main.temp_min);
+  minTemp.innerHTML = min;
+
+  let maxTemp = document.querySelector(`#max-temp`);
+  let max = Math.round(response.data.main.temp_max);
+  maxTemp.innerHTML = max;
+
+  let realFeel = document.querySelector(`#real-feel`);
+  let feelsLike = Math.round(response.data.main.feels_like);
+  realFeel.innerHTML = feelsLike;
+
+  let humidityCard = document.querySelector(`#humidity`);
+  let humidity = Math.round(response.data.main.humidity);
+  humidityCard.innerHTML = humidity;
+
+  let windSpeedCard = document.querySelector(`#wind-speed`);
+  let windSpeed = Math.round(response.data.wind.speed);
+  windSpeedCard.innerHTML = windSpeed;
+
+  let sunriseCard = document.querySelector(`#sunrise`);
+  let sunriseUnix = response.data.sys.sunrise;
+  let sunriseTime = new Date(sunriseUnix * 1000);
+  let sunriseHours = String(sunriseTime.getHours()).padStart(2, `0`);
+  let sunriseMinutes = String(sunriseTime.getMinutes()).padStart(2, `0`);
+  let sunriseSeconds = String(sunriseTime.getSeconds()).padStart(2, `0`);
+  let sunrise = `${sunriseHours}:${sunriseMinutes}:${sunriseSeconds}`;
+  sunriseCard.innerHTML = sunrise;
+
+  let sunsetCard = document.querySelector(`#sunset`);
+  let sunsetUnix = response.data.sys.sunset;
+  let sunsetTime = new Date(sunsetUnix * 1000);
+  let sunsetHours = String(sunsetTime.getHours()).padStart(2, `0`);
+  let sunsetMinutes = String(sunsetTime.getMinutes()).padStart(2, `0`);
+  let sunsetSeconds = String(sunsetTime.getSeconds()).padStart(2, `0`);
+  let sunset = `${sunsetHours}:${sunsetMinutes}:${sunsetSeconds}`;
+  sunsetCard.innerHTML = sunset;
+
+  console.log(response);
+}
+
+function search(location) {
+  let units = `metric`;
+  let apiKey = `33ff0888d56d2589acf1d714577ae3c1`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(currentTemperature);
+}
+
+search(`Quito`);
 
 function getCurrentLocation(events) {
   navigator.geolocation.getCurrentPosition(getPosition);
@@ -114,70 +127,7 @@ function getCurrentLocation(events) {
     let apiKey = `33ff0888d56d2589acf1d714577ae3c1`;
     let apiUrlCurrentLocation = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
 
-    function currentLocationTemperature(response) {
-      let currentCity = document.querySelector(`#current-city`);
-      let city = response.data.name;
-      currentCity.innerHTML = city;
-
-      let todayTemp = document.querySelector(`#today-degrees`);
-      let temp = Math.round(response.data.main.temp);
-      todayTemp.innerHTML = `${temp}°C`;
-
-      let weatherDescription = document.querySelector(
-        `#today-temp-description`
-      );
-      let description = response.data.weather[0].description;
-      weatherDescription.innerHTML = description;
-
-      let todayIcon = document.querySelector(`#today-weather-icon`);
-      todayIcon.setAttribute(
-        `src`,
-        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-      );
-      todayIcon.setAttribute(`alt`, response.data.weather[0].description);
-
-      let minTemp = document.querySelector(`#min-temp`);
-      let min = Math.round(response.data.main.temp_min);
-      minTemp.innerHTML = min;
-
-      let maxTemp = document.querySelector(`#max-temp`);
-      let max = Math.round(response.data.main.temp_max);
-      maxTemp.innerHTML = max;
-
-      let realFeel = document.querySelector(`#real-feel`);
-      let feelsLike = Math.round(response.data.main.feels_like);
-      realFeel.innerHTML = feelsLike;
-
-      let humidityCard = document.querySelector(`#humidity`);
-      let humidity = Math.round(response.data.main.humidity);
-      humidityCard.innerHTML = humidity;
-
-      let windSpeedCard = document.querySelector(`#wind-speed`);
-      let windSpeed = Math.round(response.data.wind.speed);
-      windSpeedCard.innerHTML = windSpeed;
-
-      let sunriseCard = document.querySelector(`#sunrise`);
-      let sunriseUnix = response.data.sys.sunrise;
-      let sunriseTime = new Date(sunriseUnix * 1000);
-      let sunriseHours = String(sunriseTime.getHours()).padStart(2, `0`);
-      let sunriseMinutes = String(sunriseTime.getMinutes()).padStart(2, `0`);
-      let sunriseSeconds = String(sunriseTime.getSeconds()).padStart(2, `0`);
-      let sunrise = `${sunriseHours}:${sunriseMinutes}:${sunriseSeconds}`;
-      sunriseCard.innerHTML = sunrise;
-
-      let sunsetCard = document.querySelector(`#sunset`);
-      let sunsetUnix = response.data.sys.sunset;
-      let sunsetTime = new Date(sunsetUnix * 1000);
-      let sunsetHours = String(sunsetTime.getHours()).padStart(2, `0`);
-      let sunsetMinutes = String(sunsetTime.getMinutes()).padStart(2, `0`);
-      let sunsetSeconds = String(sunsetTime.getSeconds()).padStart(2, `0`);
-      let sunset = `${sunsetHours}:${sunsetMinutes}:${sunsetSeconds}`;
-      sunsetCard.innerHTML = sunset;
-
-      console.log(response);
-    }
-
-    axios.get(apiUrlCurrentLocation).then(currentLocationTemperature);
+    axios.get(apiUrlCurrentLocation).then(currentTemperature);
   }
 }
 
